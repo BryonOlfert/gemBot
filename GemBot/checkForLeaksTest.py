62235712edf559db1ebe04502644ba9bb5739881
Bryon <blolfert@gmail.com>
2022-04-13 16:42:18 -0500

    check for leaks works(Todo implement)

--- GemBot/checkForLeaksTest.py
+++ GemBot/checkForLeaksTest.py
@@ -7,5 +7,17 @@ exec(open("centerBuilds1p.py").read())
 
 
 def main():
+    # turn on failsafe and starts
+    pyautogui.FAILSAFE = True
+    countDownTimer(2)
+    
+    while 1 == 1:
+        image = pyautogui.locateCenterOnScreen("buttons/present2.png", grayscale=False, confidence=.9, region=(1210, 478, 1301, 605))
+        time.sleep(.1)
+        if image == None:
+            print("restart boi")
+        else:
+            print("all good")
 
-    image = pyautogui.locateOnScreen("buttons/present2.png")
+
+main()
