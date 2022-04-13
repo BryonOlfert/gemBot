c689302ef4b1ff14bba7ad76cc54a883d2cdab99
Bryon <blolfert@gmail.com>
2022-04-13 16:14:12 -0500

    Start to Check for enemys hiting 2

---
+++ checkForLeaksTest.py
@@ -0,0 +1,11 @@
+exec(open("combinations.py").read())
+exec(open("functions.py").read())
+exec(open("builds.py").read())
+exec(open("gemBuilds.py").read())
+exec(open("centerBuilds1p.py").read())
+
+
+
+def main():
+
+    image = pyautogui.locateOnScreen("buttons/present2.png")
